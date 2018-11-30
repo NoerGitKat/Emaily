c30c28716abb82764ddd1c94dca2887b8dff0a9b
Noer Paanakker <noer@healthyworkers.nl>
2018-11-30 19:36:45 +0100

    added root route

--- server/server.js
+++ server/server.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res) => {
+	res.send('hello world!');
+});
+
 // Dynamic PORT binding
 const PORT = process.env.PORT || 5000;
 
